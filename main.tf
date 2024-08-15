8303e3e5fef37a8106733e9ee9da6328575bbdab
Enaide Corrales Fernández <ecfcode@gmail.com>
2024-08-15 18:29:36 +0200

    fix deploy nginx image with dockker

--- main.tf
+++ main.tf
@@ -109,9 +109,13 @@ resource "aws_instance" "myapp_server" {
     user_data = <<EOF
                     #!/bin/bash
                     sudo yum update -y && sudo yum install -y docker
-                    sudo systemctl enable docker
                     sudo systemctl start docker
                     sudo usermod -aG docker $USER
+                    
+                    # we need to restart docker to apply above change in user group
+                    sudo systemctl restart docker
+                    sudo systemctl enable docker
+                    
                     docker run -p 8080:80 nginx
                 EOF
     
