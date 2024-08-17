cefa1512d4f2e4cc86089366000127e8c6620777
Enaide Corrales Fernández <ecfcode@gmail.com>
2024-08-17 11:04:34 +0200

    modularize tf project

---
+++ modules/webserver/variables.tf
@@ -0,0 +1,8 @@
+variable vpc_id {}
+variable image_name {}
+variable avail_zone {}
+variable subnet_id {}
+variable env_prefix {}
+variable my_ip {}
+variable instance_type {}
+variable public_key_location {}
